bd2d07089cae3a1898c27879a3fe5caab4448532
Mahad Kalam <mahadkalam1234@gmail.com>
2024-12-02 19:28:52 +0000

    Tidy up some code

--- src/day2.rs
+++ src/day2.rs
@@ -1,12 +1,10 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use owo_colors::OwoColorize;
 
 #[aoc_generator(day2)]
 pub fn input_generator(input: &str) -> Vec<Vec<i32>> {
     input
         .lines()
-        .map(|line| line.split_whitespace().collect::<Vec<_>>())
-        .map(|line| line.iter().map(|n| n.parse::<i32>().unwrap()).collect())
+        .map(|line| line.split(' ').map(|n| n.parse::<i32>().unwrap()).collect())
         .collect()
 }
 
@@ -22,12 +20,7 @@ pub fn solve_part1(input: &[Vec<i32>]) -> i32 {
 
     for report in input {
         if check_report(report) {
-            /*             println!("{:?} is {}", report, "safe".green().bold());
-             */
             safe_count += 1
-        } else {
-            /*             println!("{:?} is {}", report, "unsafe".red().bold());
-             */
         }
     }
 
@@ -39,17 +32,13 @@ pub fn solve_part2(input: &[Vec<i32>]) -> i32 {
     let mut safe_count = 0;
 
     for report in input {
-        let mut safe = false;
         'report_iter: for n in 0..report.len() {
             let mut report = report.clone();
             report.remove(n);
             if check_report(&report) {
-                safe = true;
+                safe_count += 1;
                 break 'report_iter;
             }
-        }
-        if safe {
-            safe_count += 1
         }
     }
 
@@ -59,8 +48,8 @@ pub fn solve_part2(input: &[Vec<i32>]) -> i32 {
 fn check_report(report: &Vec<i32>) -> bool {
     let mut mode: Option<Op> = None;
     let mut safe = true;
-    for window in report.windows(2).map(|window| (window[0], window[1])) {
-        let n = window.0 - window.1;
+    for window in report.windows(2) {
+        let n = window[0] - window[1];
         let mode = mode
             .get_or_insert_with(|| if n > 0 { Op::Decr } else { Op::Incr })
             .clone();
